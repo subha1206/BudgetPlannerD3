8fb489a6d0fcf96f4e261d4128e7c384ada35659
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-05-16 18:19:48 +0530

    added some styling to the chart and add tool tip for the slices and also added event handlr for the interectivity

--- graph.js
+++ graph.js
@@ -5,7 +5,8 @@ const svg = d3
   .select(".canvas")
   .append("svg")
   .attr("width", dims.width + 150)
-  .attr("height", dims.heigh + 150);
+  .attr("height", dims.heigh + 20)
+  .attr("class", "z-depth-5");
 
 const group = svg
   .append("g")
@@ -25,25 +26,77 @@ const arcPath = d3
 
 const randColor = d3.scaleOrdinal(d3.schemeCategory10);
 
+// Data legend setup
+const LegendGroup = svg
+  .append("g")
+  .attr("transform", `translate(${dims.width + 20}, 10)`);
+
+const legend = d3
+  .legendColor()
+  .shape("circle")
+  .shapePadding(10)
+  .scale(randColor);
+
+// Creating Tool tip using d3-tip
+
+const tip = d3
+  .tip()
+  .attr("class", "tip card")
+  .html((d) => {
+    let content = `<div class="name">${d.data.name}</div>`;
+    content += `<div class="cost">${d.data.cost}</div>`;
+    content += `<div class="delete">Click on slice to delete</div>`;
+    return content;
+  });
+
+group.call(tip);
+
 // updateData function(realTime listner for any changes in database)
 const updateData = (data) => {
   // update color scale
   randColor.domain(data.map((item) => item.name));
 
+  LegendGroup.call(legend);
+  LegendGroup.selectAll("text").attr("fill", "#fff");
+
   //   joining pie data to path element
   const paths = group.selectAll("path").data(pie(data));
 
+  // update and delete arc using exit selection
   paths.exit().remove();
-  paths.attr("d", arcPath);
+
+  paths
+    .attr("d", arcPath)
+    .transition()
+    .duration(1000)
+    .attrTween("d", arcTweenUpdate);
 
   paths
     .enter()
     .append("path")
     .attr("class", "arc")
-    .attr("d", arcPath)
     .attr("stroke", "#fff")
-    .attr("stroke-width", 3)
-    .attr("fill", (d) => randColor(d.data.name));
+    .attr("stroke-width", 2)
+    .attr("fill", (d) => randColor(d.data.name))
+    .each(function (d) {
+      this._current = d;
+    })
+    .transition()
+    .duration(1000)
+    .attrTween("d", arcTweenEnter);
+
+  // add events
+  group
+    .selectAll("path")
+    .on("mouseover", (d, i, n) => {
+      tip.show(d, n[i]);
+      handelMouseOver(d, i, n);
+    })
+    .on("mouseout", (d, i, n) => {
+      tip.hide();
+      handelMouseOut(d, i, n);
+    })
+    .on("click", handelClick);
 };
 
 // dataArray and firestore(information from the database)
@@ -70,3 +123,44 @@ db.collection("expenses").onSnapshot((res) => {
   });
   updateData(data);
 });
+
+const arcTweenEnter = (d) => {
+  let i = d3.interpolate(d.endAngle, d.startAngle);
+
+  return function (t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
+
+function arcTweenUpdate(d) {
+  //   interpoltae b/w two objects
+  let i = d3.interpolate(this._current, d);
+
+  // update the current path
+  this._current = i(1);
+
+  return function (t) {
+    return arcPath(i(t));
+  };
+}
+
+// event handler
+
+const handelMouseOver = (d, i, n) => {
+  d3.select(n[i]).transition("changeFill").duration(300).attr("fill", "coral");
+};
+
+const handelMouseOut = (d, i, n) => {
+  d3.select(n[i])
+    .transition("changeFill")
+    .duration(300)
+    .attr("fill", (d) => randColor(d.data.name));
+};
+
+// delete record funtion
+
+const handelClick = (d) => {
+  const id = d.data.id;
+  db.collection("expenses").doc(id).delete();
+};
